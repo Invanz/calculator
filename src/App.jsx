155214fb7c6526b801efd2a91149b77a87d9028d
Arturo Guerra <arturoalejandroguerram@gmail.com>
2023-10-25 10:37:53 -0500

    se crea proyecto de React de calculadora

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useReducer } from "react";
+import DigitButton from "./components/DigitButton";
+import OperationButton from "./components/OperarionButton";
+import "./styles.css";
+import Calculator from "./components/Calculator";
+
+function App() {
+
+  return (<>
+  <h1 className="title">This is just a calculator.</h1>
+  < Calculator />
+  </>
+  )
+}
+
+export default App
